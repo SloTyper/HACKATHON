d3082afd898d8a0e4e69e2a30afa0f716b5ba650
SloTyper <harishselva1213@gmail.com>
2022-06-10 18:21:27 +0530

    a

--- routes/auth.js
+++ routes/auth.js
@@ -44,8 +44,8 @@ router.post("/login", (req, res, next) => {
 
   router.post("/register", (req, res) => {
     console.log("LLLLLLLLLL");
-    console.log("req.body", req.body);
-    res.send("yeyeyeye", req.body);
+    console.log("req.body", req.body.username);
+    res.send(req.body);
     // User.findOne({ username: req.body.username }, async (err, doc) => {
     //   if (err) throw err;
     //   if (doc) res.send("User Already Exists");
