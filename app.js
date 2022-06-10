d077be4a6a98bb9b5aaf4702a5f41770a393c7c8
SloTyper <harishselva1213@gmail.com>
2022-06-11 02:48:45 +0530

    hahaha

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ app.use(
     })
   );
 
+  app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+  
 const secret = process.env.SECRET;
 
 app.use(cookieParser(secret));
