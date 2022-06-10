3bd41909ff5304849ab811aaf7c9207139d6b61f
SloTyper <harishselva1213@gmail.com>
2022-06-11 05:26:36 +0530

    new

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://62a3b7f860f8711bc200727d--zesty-axolotl-e7aeb7.netlify.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
