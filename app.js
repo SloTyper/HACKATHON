a72f2ea67fbeb807e7b7cbb61a5fc02bec435109
SloTyper <harishselva1213@gmail.com>
2022-06-10 22:49:42 +0530

    ya

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 
 app.use(
     cors({
-      origin: ["http://localhost:3000"],
+      origin: ["http://localhost:3000", "*"],
       credentials: true,
       methods: ["GET", "POST"],
     })
